ee57cf851cbe96fd570271446b1cb9796a2d1084
Xander Aerts <xanderaerts@gmail.com>
2023-09-25 15:02:21 +0200

    w1 oef1 test1

---
+++ W1/Duplicates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1
+{
+    internal class Duplicates
+    {
+        public string inputStr { get; set; }
+
+        public List<string> findDubble(string[] array, string inputStr)
+        {
+
+            List<string> matches = new List<string>();
+
+            for (int i = 0; i < array.Length; i++) {
+
+                string firstChar = array[i].Substring(0, 2);
+
+
+                if (firstChar == inputStr) {
+                    matches.Add(array[i]);                    
+                }            
+            }
+
+            if (matches.Count > 0)
+            {
+                return matches;
+            }
+            else {
+                matches.Add("no match");
+                return matches;
+            }
+
+        }
+    }
+}
---
+++ W1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace W1
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            string[] array = File.ReadAllLines("words.txt");
+
+            Duplicates dp = new Duplicates();
+
+
+            Console.WriteLine("Give 2 characters: ");
+
+            string inputStr = Console.ReadLine();
+
+            List<string> doubles = dp.findDubble(array, inputStr);
+
+
+            for (int i = 0; i < doubles.Count; i++) {
+                Console.WriteLine($"{doubles[i]}");
+            }
+
+
+        }
+    }
+}
