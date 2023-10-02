dc123d5e45dcd49b23db2daf00ddc92f683909a5
xanderaerts <xanderaerts@gmail.com>
2023-10-02 15:23:05 +0200

    eppic fail Lyntor

---
+++ Week1/Missing/Missing.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Missing
+{
+    public class Missing
+    { 
+
+        public int findMissing(List<int> input){
+            return -1;
+        }
+    }
+}
--- Week1/Missing/Program.cs
+++ Week1/Missing/Program.cs
@@ -6,12 +6,27 @@ namespace Missing
     {
         static void Main(string[] args)
         {
-            
-        
-            
             Console.WriteLine("Give a sorrted array of number");
 
             string input = Console.ReadLine();
+
+            if(input == null){
+                input = "empty";
+            }
+
+            string[] inputNumbersSplitted = input.Split(' ');
+            List<int> inputNumbersInt = new List<int>();
+
+            foreach(string nr in inputNumbersSplitted){
+                try{
+                    int parsed = Int32.Parse(nr);
+                    inputNumbersInt.Add(parsed);
+                }
+                catch{
+                    Console.WriteLine("Somthing went wrong, you didn't give a number");
+                    break;
+                } 
+            }
 
             
         }
