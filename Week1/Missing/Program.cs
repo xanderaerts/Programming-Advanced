27ae364c6ff642536251c1c4d802c1ccc5a606b1
xanderaerts <xanderaerts@gmail.com>
2023-10-02 10:49:50 +0200

    start missing

---
+++ Week1/Missing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Missing 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+            
+            Console.WriteLine("Give a sorrted array of number");
+
+            string input = Console.ReadLine();
+
+            
+        }
+    }
+}
