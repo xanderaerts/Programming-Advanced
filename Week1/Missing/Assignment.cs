4cc5b2f2083a14ca132689c1429644ce033e3261
xanderaerts <xanderaerts@gmail.com>
2023-10-10 08:59:56 +0200

    it worksgit add .

--- Week1/Missing/Assignment.cs
+++ Week1/Missing/Assignment.cs
@@ -5,16 +5,17 @@ namespace Module_01
     public class Assignment
     { 
         public string Missing(int[] array){
-            /* double missing = -1,sum = 0,expected = 0;
+            double missing = -1,sum = 0,expected = 0;
 
              for (int i = 0; i < array.Length; i++) {
                  sum += array[i];
              }
 
-             expected = ((double)(array.Length + 1) / 2) * (array[0] + array[array.Length - 1]);
 
-             Console.WriteLine(expected);
-             Console.WriteLine(sum);
+            int lenList =  (array[array.Length-1] - array[0]) + 1;
+
+             expected = ((double)(lenList) / 2) * (array[0] + array[array.Length - 1]);
+
 
              missing = expected - sum;
 
@@ -23,26 +24,7 @@ namespace Module_01
                  return "No missing number.";
              }
              else
-             { return missing.ToString(); } */
-
-            int missing = -1, one, two;
-
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                one = array[i];
-                two = array[i + 1];
-
-                if ((two - one) > 1)
-                {
-                    missing = one + 1;
-                    break;
-                }
-            }
-
-            if (missing == -1) { return "No missing number."; }
-            else { return missing.ToString(); }
-
-
+             { return missing.ToString(); } 
         }
     }
 }
--- Week1/Missing/Program.cs
+++ Week1/Missing/Program.cs
@@ -15,18 +15,18 @@ namespace Module_01
             int[] inputNumbersInt = new int[inputNumbersSplitted.Length];
             int i = 0;
 
-                try{
-                    foreach(string nr in inputNumbersSplitted){
-                        int parsed = Int32.Parse(nr);
-                        inputNumbersInt[i] = parsed;
-                        i++; 
-                    }  
-                    
-                    Console.WriteLine(missing.Missing(inputNumbersInt));
-                }
-                catch { 
-                    Console.WriteLine("Crazy input!");
-                }
+            try{
+                foreach(string nr in inputNumbersSplitted){
+                    int parsed = Int32.Parse(nr);
+                    inputNumbersInt[i] = parsed;
+                    i++; 
+                }  
+                
+                Console.WriteLine(missing.Missing(inputNumbersInt));
+            }
+            catch { 
+                Console.WriteLine("Crazy input!");
+            }       
 
 
             
