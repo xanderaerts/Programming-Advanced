d43c8621299eee08d89ad1f8dacd7dffe1515cfe
xanderaerts <xanderaerts@gmail.com>
2023-10-13 11:11:52 +0200

    vb w4

---
+++ VoorbeeldenLes/w4/recursion/FindKey.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+
+namespace recursion
+{
+    public class FindKey
+    {
+        public int count {get;set;}
+
+        public string Algorithm1(string folder){
+
+            Queue<string> folders = new Queue<string>();
+            
+            folders.Enqueue(folder);
+
+            while(folders.Count > 0){
+                count++;
+                folder = folders.Dequeue();
+
+                if(Directory.GetFiles(folder).Length > 0) return folder;
+                else{
+                    foreach (string dir in Directory.GetDirectories(folder)){
+                        folders.Enqueue(dir);
+                    }
+                }
+            }
+
+            return "not found";
+
+        }
+
+        public string Algorithm2(string folder){
+            foreach(string entry in Directory.GetFileSystemEntries(folder)){
+                count++;
+                if(Directory.Exists(entry)){
+                   string value = Algorithm2(entry);
+                   if(value != "not found"){return value;}
+                }
+                else if(File.Exists(entry)){
+                    return entry;
+                }
+            }
+
+            return "not found";
+        }
+    }
+
+}
---
+++ VoorbeeldenLes/w4/recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿using recursion;
+using TM.ProgrammingAdvanced;
+
+namespace Recursion{
+    internal class Program{
+        static void Main(string[] args){
+            string folder = @"./brol";
+
+            FindKey keyfinder = new FindKey();
+            Console.WriteLine("Key found! -->", keyfinder.Algorithm1(folder) + "Count" + keyfinder.count);
+
+
+            keyfinder.count = 0;
+            Console.WriteLine("Key found! -->", keyfinder.Algorithm2(folder) + "Count" + keyfinder.count);
+
+        }
+
+    }
+}
