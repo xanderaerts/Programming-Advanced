1d4537a29add853f244e8fcbc16d878775fc5936
xanderaerts <xanderaerts@gmail.com>
2023-11-03 11:02:52 +0100

    vb backtracking

--- VoorbeeldenLes/w5/BackTracking/Program.cs
+++ VoorbeeldenLes/w5/BackTracking/Program.cs
@@ -13,13 +13,13 @@ namespace BackTracking{
 
             Subset subset = new Subset();
 
-            List<List<int>> results =  subset.Solve(new int[]{1,2,3,4});
+            //List<List<int>> results =  subset.Solve(new int[]{1,2,3,4});
             List<List<int>> results2 = subset.SolveBinary(new int[]{4,8,10,489,44,45});
 
-            foreach(var item in results){
+/*            foreach(var item in results){
                 Console.WriteLine(string.Join(" ",item));
             }
-
+*/
             foreach(var item in results2){
               //  Console.WriteLine(string.Join(" ",item));
             }
