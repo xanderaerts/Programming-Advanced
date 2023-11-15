1a15ff0a37c89d098e7ffe0c540902b12eac0335
Xander Aerts <xanderaerts@gmail.com>
2023-11-15 11:50:11 +0100

    subwords

---
+++ Week6/Words/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Module_06{
+    internal class Program{
+        static void Main(string[] args){
+            
+            string[] words = File.ReadAllLines("english.txt");
+
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            
+
+
+            Assignment assignment = new Assignment();
+            
+            try{
+                Console.WriteLine(assignment.SearchWords(words,input));
+            }
+            catch{
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
