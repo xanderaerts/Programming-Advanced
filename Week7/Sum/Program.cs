5e93eb3ea32f96ee6c2666a0a77be811c6c71b31
xanderaerts <xanderaerts@gmail.com>
2023-11-24 16:31:30 +0100

    sum sum sum

---
+++ Week7/Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Module_07{
+    internal class Program{
+        static void Main(string[] args){
+
+            try{
+                int input = Int32.Parse(Console.ReadLine());
+                
+                if(input < 0) throw new Exception();
+
+                Assignment assignment = new Assignment();
+                Console.WriteLine(assignment.Sum(input));
+            }
+            catch{
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
