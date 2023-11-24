094485081334026157de9c3293a23cd1552dc9e1
xanderaerts <xanderaerts@gmail.com>
2023-11-24 09:54:58 +0100

    fix alphabet

---
+++ Week7/BinaryAlphabet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module_07 {
+    internal class Program{
+        static void Main(string[] args){
+            
+            try{
+                int input = Int32.Parse(Console.ReadLine());
+
+                Assignment assignment = new Assignment();
+
+                Console.WriteLine(assignment.PrintAlphabet(input));
+            }
+            catch{
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
