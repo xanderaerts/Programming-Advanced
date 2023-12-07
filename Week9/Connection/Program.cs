15e466a7f3ee50df87fc403ff7da14e7e9d31367
Xander Aerts <xanderaerts@gmail.com>
2023-12-07 22:56:15 +0100

    connection

---
+++ Week9/Connection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Module_09{
+    internal class Program{
+        static void Main(string[] args){
+
+            try{
+                Assignment assignment = new Assignment();
+                assignment.start();
+            }
+
+            catch{
+                Console.WriteLine("Crazy input!");
+            }
+        }
+    }
+}
